56c2a62891f64ae1d76305736b6d2b4fa3d30737
suvas <ss5138275@gmail.com>
2026-08-28 22:23:42 +0545

    Fix CORS: handle wildcard and allow all origins

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,8 +6,25 @@ DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-CORS_ALLOW_ALL_ORIGINS = True
-# SSL / Security
+
+# ----- CORS Configuration -----
+# For mobile apps, we can allow all origins.
+# But if CORS_ALLOWED_ORIGINS is set to '*' (invalid), we override it.
+cors_allowed = os.environ.get('CORS_ALLOWED_ORIGINS', '')
+if cors_allowed == '*':
+    # Invalid wildcard – ignore it and allow all origins
+    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOWED_ORIGINS = []
+else:
+    # Parse comma‑separated list of valid origins
+    CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_allowed.split(',') if origin.strip()]
+    CORS_ALLOW_ALL_ORIGINS = False
+
+# If you want to allow all origins regardless, just set:
+# CORS_ALLOW_ALL_ORIGINS = True
+# And remove CORS_ALLOWED_ORIGINS from environment variables.
+
+# ----- SSL / Security -----
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
